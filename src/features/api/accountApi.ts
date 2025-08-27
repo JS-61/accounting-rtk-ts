ef07e1fc2a81a9afe5edad65938a2d1d2faa1eb9
MariiaKpln <kpln.mary@gmail.com>
2025-08-27 12:36:59 +0300

    change to correct fetchUser

--- src/features/api/accountApi.ts
+++ src/features/api/accountApi.ts
@@ -27,23 +27,25 @@ export const registerUser = createAsyncThunk(
 
 export const fetchUser = createAsyncThunk(
     'user/fetch',
-    async(user: UserRegister) => {
+    async ({ login, password }: { login: string, password: string }) => {
+        const token = createToken(login, password);
+
         const response = await fetch(`${base_url}/account/login`, {
             method: "POST",
             headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Basic dXNlci1qb2huOjEyMzQ=',
+                'Authorization': token,
             },
-            body: JSON.stringify(user),
         });
-        if (response.status === 401) {
-            throw new Error(`User ${user.login}  Unauthorized`);
 
+        if (response.status === 401) {
+            throw new Error(`User ${login} Unauthorized`);
         }
+
         if (!response.ok) {
             throw new Error(`Something went wrong`);
         }
+
         const data = await response.json();
-        const token = createToken(user.login, user.password)
-        return {user: data, token}
-    })
+        return { user: data, token };
+    }
+);
